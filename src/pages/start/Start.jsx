7da6a62f8c5edf3a1b632204edf430654d8befb5
tessavermeulen07 <tessavermeulen@me.com>
2026-01-05 16:31:23 +0100

    react-icons geinstalleerd en start CSS en JSX aangepast

--- src/pages/start/Start.jsx
+++ src/pages/start/Start.jsx
@@ -1,6 +1,7 @@
 import "./Start.css";
 import NavStart from "../../nav-start/NavStart.jsx";
 import boekify from "../../assets/boekify-afbeelding.jpg";
+import 
 
 
 function Start() {
@@ -16,7 +17,21 @@ function Start() {
                     <h4>Voor lezers die hun boeken koesteren</h4>
                 </div>
             </section>
-
+            <div className="container-articles">
+                <article className="article-box">
+                    <h3>Find</h3>
+                    <p>dfhdfhjs</p>
+                </article>
+                <article className="article-box">
+                    <h3>Track</h3>
+                    <p>dfhdfhjs</p>
+                </article>
+                <article className="article-box">
+                    <h3>Add</h3>
+                    
+                    <p>dfhdfhjs</p>
+                </article>
+            </div>
         </>
     )
 }
