f3a82a77b842cbba0d79c1f5fe05e55d664fdf04
tessavermeulen07 <tessavermeulen@me.com>
2026-02-16 10:38:38 +0100

    Verder gewerkt aan Library. Door middel van de API de boekenlijst toegevoegd aan de pagina.

--- src/pages/library/Library.jsx
+++ src/pages/library/Library.jsx
@@ -1,15 +1,43 @@
 import './Library.css';
 import Navigation from '../../navigation/Navigation.jsx';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 
 function Library() {
 
-    // async function getBooks() {
-    //     const resultBooks =
-    // }
+    const [library, setLibrary] = useState(null);
+    const [loading, toggleLoading] = useState(false);
+    const [error, toggleError] = useState(false);
+
+    async function getBooks() {
+        try {
+            const resultBooks = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/books', {
+                headers: {
+                    'novi-education-project-id': '268aff3c-ae58-411a-a55f-e0c1ec05146d'
+                }
+            });
+
+            resultBooks.data.sort((a, b) => {
+                return a - b
+            })
+
+            setLibrary(resultBooks.data);
+            console.log(resultBooks);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    useEffect(() => {
+        void getBooks();
+    }, []);
+
+
 
     return (
         <>
-            <Navigation />
+            <Navigation/>
 
             <div className="main-content-library">
                 <div className="left-content-library">
@@ -25,7 +53,27 @@ function Library() {
                     </div>
                 </div>
                 <div className="right-content-library">
-                    <div className="banner-library">A-balk</div>
+                    {/*<div className="inside-right-content-library">*/}
+                    {/*    <span className="banner-library">A-balk</span>*/}
+                    {/*    <span className="list-books-library">Book 1</span>*/}
+                    {/*    <span className="list-books-library">Book 2</span>*/}
+                    {/*</div>*/}
+                    {/*<div className="inside-right-content-library">*/}
+                    {/*    <span className="banner-library">B-balk</span>*/}
+                    {/*    <span className="list-books-library">Book 1</span>*/}
+                    {/*</div>*/}
+                    <div className="inside-right-content-library">
+                            <ul>
+                                {library?.map((books) => {
+                                    return (
+                                        <li key={books.title} className="list-books-library">
+                                            <p>{books.title}</p>
+                                        </li>
+                                    );
+                                }
+                                    )}
+                            </ul>
+                    </div>
                 </div>
             </div>
 
