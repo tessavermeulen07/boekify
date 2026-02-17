7c2a69033795591f261da17500f6aa7d35825a67
tessavermeulen07 <tessavermeulen@me.com>
2026-02-17 12:39:53 +0100

    AddBooks.css aangepast met een class om het formulier goed te plaatsen op de pagina, AddBooks.jsx aangepast -> formulier toegevoegd.

--- src/pages/addBooks/AddBooks.jsx
+++ src/pages/addBooks/AddBooks.jsx
@@ -2,60 +2,72 @@ import './AddBooks.css';
 import Navigation from '../../navigation/Navigation.jsx';
 import axios from 'axios';
 import {useState} from 'react';
+import TextLabel from '../../components/textLabel/TextLabel.jsx';
+import ButtonNavStart from '../../components/button-nav-start/ButtonNavStart.jsx';
 
 function AddBooks() {
 
-    const [loading, toggleLoading] = useState(false);
-    const [error, toggleError] = useState(false);
-    const [wrongTitle, setWrongTitle] = useState('');
-    const [query, setQuery] = useState('');
-
-    //helper functie schrijven spatie vervangen voor + teken
-    // template literals
-
-    async function searchBooks() {
-        try {
-            const resultBookSearch = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/books', {
-                'novi-education-project-id': '268aff3c-ae58-411a-a55f-e0c1ec05146d'
-            });
-            console.log(resultBookSearch);
-            // setCountrySearch(resultBookSearch.data[0]);
-        } catch (error) {
-            console.error(error);
-            // toggleError(true);
-            // setWrongName(query);
-        } finally {
-            toggleLoading(false);
-        }
-    }
-
-    const handleSearch = (event) => {
-        event.preventDefault();
-        searchBooks('');
-        setQuery('');
-    }
+    const [titleOfBook, setTitleOfBook] = useState('');
+    const [authorOfBook, setAuthorOfBook] = useState('');
+    const [isbnOfBook, setIsbnOfBook] = useState('');
+    const [genreOfBook, setGenreOfBook] = useState('')
+    const [priceOfBook, setPriceOfBook] = useState('');
 
     return (
         <>
             <Navigation/>
-
-            <form>
-                <label htmlFor="booksearch" onSubmit={handleSearch}>
-                    <input
-                        type="text"
-                        id="booksearch"
-                        name="booksearch"
-                        size="50"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                </label>
-                <button
-                    type="submit"
-                    disabled={loading}
-                >
-                    Search
-                </button>
+            <div>
+                <h2>Voeg een boek toe</h2>
+            </div>
+            <form className="form-add-books">
+                <TextLabel
+                    startTextLabel="Titel"
+                    typeOfLabel="text"
+                    idOfLabel="title"
+                    nameOfLabel="title"
+                    valueOfLabel={titleOfBook}
+                    onChangeOfLabel={(e) => setTitleOfBook(e.target.value)}
+                />
+                <TextLabel
+                    startTextLabel="Auteur"
+                    typeOfLabel="text"
+                    idOfLabel="author"
+                    nameOfLabel="author"
+                    valueOfLabel={authorOfBook}
+                    onChangeOfLabel={(e) => setAuthorOfBook(e.target.value)}
+                />
+                <TextLabel
+                    startTextLabel="ISBN"
+                    typeOfLabel="text"
+                    idOfLabel="isbn"
+                    nameOfLabel="isbn"
+                    valueOfLabel={isbnOfBook}
+                    onChangeOfLabel={(e) => setIsbnOfBook(e.target.value)}
+                />
+                <TextLabel
+                    startTextLabel="Genre"
+                    typeOfLabel="text"
+                    idOfLabel="genre"
+                    nameOfLabel="genre"
+                    valueOfLabel={genreOfBook}
+                    onChangeOfLabel={(e) => setGenreOfBook(e.target.value)}
+                />
+                {/*Of voor de genres een select menu maken met de ID's er al in, zodat dit makkelijker toegevoegd kan worden*/}
+                <TextLabel
+                    startTextLabel="Prijs"
+                    typeOfLabel="number"
+                    idOfLabel="price"
+                    nameOfLabel="price"
+                    valueOfLabel={priceOfBook}
+                    onChangeOfLabel={(e) => setPriceOfBook(e.target.value)}
+                />
+                <ButtonNavStart
+                    typeOfButton="send"
+                    valueOfButton="send"
+                    nameOfButton="add-book"
+                    // onClickOfButton={}
+                    textOnButton="Voeg toe"
+                />
             </form>
 
         </>
